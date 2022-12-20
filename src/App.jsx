13dd5881ccf6ebb084b672a39a3ebd81856cc41c
Gabriel0109 <gabrielglfg@outlook.com>
2022-12-20 15:35:00 -0300

    hotfix: ajustes das cores do tema da prefeitura e add qrcode

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,5 @@ function App() {
   )
 }
 
-// { !code ? <SearchMain /> : }
 
 export default App
--- src/hooks/getCode.jsx
+++ src/hooks/getCode.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import {toast } from 'react-toastify'
 
 import axios from 'axios'
 
@@ -12,6 +13,8 @@ export const CodeContext = createContext()
 export function CodeProvider({ children }) {
     const [code, setCode] = useState('')
     const [searchParams, setSearchParams] = useSearchParams();
+    const [codeExists, setCodeExists] = useState(false)
+    const [toggle, setToggle] = useState(false)
 
 
     // const searchHandler = (query) => {
@@ -24,9 +27,31 @@ export function CodeProvider({ children }) {
             setCode(urlCode.toUpperCase)
         }
     }, [])
+    // CHECA SE CÓDIGO PESQUISADO EXISTE
+    function checkCode() {
+        axios.get("https://api.mobilidade.rio/gtfs/stops/?stop_code=" + code.toUpperCase())
+            .then(response => {
+                if (response.data.count == 0 && code.length == 4) {
+                    toast.error(`O código ${code.toUpperCase()} não existe`, {
+                        position: "top-right",
+                        autoClose: 5000,
+                        hideProgressBar: true,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        theme: "colored",
+                    });
+                } else {
+                    setCodeExists(true)
+                }
+            })
 
+    }
+    useEffect(() => {
+        checkCode()
+    }, [code])
     return (
-        <CodeContext.Provider value={{ code, setCode,  setSearchParams }}>
+        <CodeContext.Provider value={{ code, setCode,  setSearchParams, codeExists, toggle, setToggle }}>
             {children}
         </CodeContext.Provider>
     )
