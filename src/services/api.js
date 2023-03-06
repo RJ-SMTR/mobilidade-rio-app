a328ada56c505a9a350055b1a07eebd7044a04f5
Gabriel0109 <gabrielglfg@outlook.com>
2023-03-06 13:25:41 -0300

    fix/apontando para api de staging no ambiente de staging

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ let baseURL = '';
 if (window.location.hostname === 'mobilidade.rio') {
     baseURL = 'https://api.mobilidade.rio/gtfs'
 } else if (window.location.hostname === 'app.staging.mobilidade.rio') {
-    baseURL = 'https://api.dev.mobilidade.rio/gtfs'
+    baseURL = 'https://api.staging.mobilidade.rio/gtfs'
 } else if(window.location.hostname === 'localhost') {
     baseURL = 'https://api.dev.mobilidade.rio/gtfs'
 }
