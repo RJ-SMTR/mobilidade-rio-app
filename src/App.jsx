8b80b81d55b7501364ed0fe6a668ebb7897e7f9f
Gabriel Guimarães <gabrielglfg@outlook.com>
2023-03-28 14:59:09 -0300

    feat: google analytics tracker (#83)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Home } from "./pages/Home"
 import { SearchMain } from './pages/SearchMain'
 import './assets/styles/globals.scss'
-import {useContext, useEffect} from 'react'
-import { CodeContext } from "./hooks/getCode"
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { useEffect} from 'react'
+import { Route, Routes} from 'react-router-dom'
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-0VNNWR1TFR"
+ReactGA.initialize(TRACKING_ID);
+
+
 
 
 function App() {
-  const {code} = useContext(CodeContext)
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
+  }, []);
 
   return (
     <Routes>
