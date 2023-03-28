92cd0830bb8367b790e9e8025ae6019f7bced26a
Gabriel0109 <gabrielglfg@outlook.com>
2023-03-28 15:15:46 -0300

    fix: google tag manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import { Home } from "./pages/Home"
 import { SearchMain } from './pages/SearchMain'
 import './assets/styles/globals.scss'
-import { useEffect} from 'react'
 import { Route, Routes} from 'react-router-dom'
-import ReactGA from 'react-ga';
-const TRACKING_ID = "G-0VNNWR1TFR"
-ReactGA.initialize(TRACKING_ID);
+
 
 
 
 
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname);
-  }, []);
+
 
   return (
     <Routes>
